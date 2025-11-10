bff21f0191fbde35fa78a9622eb687beb00dfe78
didirdt <didi.rudiatd12@gmail.com>
2025-11-10 14:57:12 +0700

    add goroutine and channel golang in notifications

--- usecase/notification_usecase.go
+++ usecase/notification_usecase.go
@@ -19,14 +19,30 @@ func NewNotificationUsecase() NotificationUsecase {
 type notificationUsecase struct{}
 
 func (b *notificationUsecase) SendNotification(notifications []entities.SendNotif) (notif_responses []entities.SendNotifResponse, er error) {
-	for _, notification := range notifications {
-		notifrepo, _ := notificationrepo.NewNotificationRepo()
-		notif_response, er := notifrepo.SendNotification(notification)
-		if er != nil {
-			notif_response.Message = "error while Send Notification : " + er.Error()
-		}
-		notif_responses = append(notif_responses, notif_response)
+	type resp struct {
+		index int
+		r     entities.SendNotifResponse
 	}
+
+	resultsCh := make(chan resp, len(notifications))
+	notif_responses = make([]entities.SendNotifResponse, len(notifications))
+	for i, notification := range notifications {
+		i2, n := i, notification
+		go func(idx int, notif entities.SendNotif) {
+			notifrepo, _ := notificationrepo.NewNotificationRepo()
+			notif_response, err := notifrepo.SendNotification(notif)
+			if err != nil {
+				notif_response.Message = "error while Send Notification : " + err.Error()
+			}
+			resultsCh <- resp{index: idx, r: notif_response}
+		}(i2, n)
+	}
+
+	for i := 0; i < len(notifications); i++ {
+		rr := <-resultsCh
+		notif_responses[rr.index] = rr.r
+	}
+	close(resultsCh)
 
 	return notif_responses, er
 }
